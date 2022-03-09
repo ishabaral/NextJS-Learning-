aa7664437a8afa7f2fd6091b70fbda9ff9fe2e87
ishabaral <baralisha.16@gmail.com>
2022-03-09 15:37:37 +0545

    getStatic props && getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Pre-rendering</h1>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+
+      <Link href='/posts'>
+        <a>Posts</a>
+      </Link>
     </>
   )
 }
---
+++ pages/posts/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link"
+
+function PostList({ posts }) {
+    return (
+        <>
+            <h2>List of Posts</h2>
+            {
+                posts.map(post => {
+                    return (
+                        <div key={post.id}>
+                            <Link href={`posts/${post.id}`} passHref >
+                                <h3>{post.id} {post.title}</h3>
+                            </Link>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default PostList
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data.slice(0, 3)
+        }
+    }
+}
