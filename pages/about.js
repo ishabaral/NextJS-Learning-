621f3b41e6a61a117bcb1b6c9d6ac9140830f548
ishabaral <baralisha.16@gmail.com>
2022-03-13 15:45:38 +0545

    styling: global, component-level, sass, css-in-js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import styles from '../styles/About.module.scss'
+
+function About() {
+    return(
+        <>
+            <h2 className={styles.highlightscss}>About</h2>
+            <button className='btn btn-primary'>Primary</button>
+        </>
+    )
+}
+
+export default About 
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Contact.module.scss'
+
+function Contact() {
+    return(
+        <>
+            <h2 className= {styles.highlightscss}>Contact</h2>
+            <button className='btn btn-primary'>Primary</button>
+        </>
+    )
+}
+
+export default Contact 
