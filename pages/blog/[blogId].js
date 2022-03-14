707a9ccfbe10ebafd42bc15c55ebf3896a201b37
ishabaral <baralisha.16@gmail.com>
2022-03-14 17:13:37 +0545

    env variables

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -7,22 +7,18 @@ return (
             <title>{title}</title>
             <meta name= 'description' content={description} />
         </Head>
-        <h1 className='content'>Blog</h1>
+        <h1 className='content'>Blog id: {process.env.NEXT_PUBLIC_ANALYTICS_ID}</h1>
     </>
 )
 }
 export default Blog
 
-export async function getStaticPaths() {
-    return {
-        paths: [
-            { params : { blogId: '1' }}
-        ],
-        fallback: false,
-    }
-}
+export async function getServerSideProps() {
+    const user = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
 
-export async function getStaticProps() {
+    console.log(`Connecting to db :  username: ${ user}, Pass: ${password}`)
+
     return {
         props: {
             title: 'Blog Title',
