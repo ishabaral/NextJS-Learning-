822d6664d351c3d9ee20c228ce4b17d60632fbe9
ishabaral <baralisha.16@gmail.com>
2022-03-15 14:53:38 +0545

    client side authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Provider } from 'next-auth/client'
 import '../styles/globals.css'
+import '../components/Navbar.css'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Provider>
+        <Navbar />
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  )
 }
 
 export default MyApp
